1459406a97101076c7f6083bc9ffd18b67d8a085
Victor Tran <victor-likes-coding@tempestas.io>
2023-02-04 01:42:10 -0500

    satisfy MVP

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Banner = (props) => {
     return (
@@ -8,7 +9,9 @@ const Banner = (props) => {
                     <h2 className="header__subheader">Your favorite food, delivered while coding</h2>
                 </div>
             </div>
-            <div className="banner__cta">Pizza?</div>
+            <Link to="/pizza" className="banner__cta" id="#order-pizza">
+                Pizza?
+            </Link>
         </div>
     );
 };
