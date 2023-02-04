97f236a835e01cc50103d8081a665b36885acd0d
Victor Tran <victor-likes-coding@tempestas.io>
2023-02-04 01:38:45 -0500

    add made components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Header from "./components/Header";
+import Main from "./components/Main";
+import Home from "./components/Home";
 
 const App = () => {
     return (
         <>
-            <Header />
-            <Switch>
-                <Route path="/" exact></Route>
-            </Switch>
+            <Header></Header>
+            <Main>
+                <Switch>
+                    <Route path="/" exact>
+                        <Home />
+                    </Route>
+                </Switch>
+            </Main>
         </>
     );
 };
