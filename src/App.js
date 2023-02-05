7da748d88aaf98d7ce98a0977dd972e96e4ed367
Victor Tran <victor-likes-coding@tempestas.io>
2023-02-04 19:44:47 -0500

    add data to home component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { Switch, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Home from "./components/Home";
+import { places } from "./data/places";
 
 const App = () => {
+    const [foodPlaces] = useState(places);
     return (
         <>
             <Header></Header>
             <Main>
                 <Switch>
                     <Route path="/" exact>
-                        <Home />
+                        <Home data={foodPlaces} />
                     </Route>
                 </Switch>
             </Main>
